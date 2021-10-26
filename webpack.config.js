9108979802b6ecefd3247cf205828024e03a4f94
bxxmi <bmi6638@gmail.com>
2021-10-26 10:41:39 +0900

    feat: 기능 설명을 위한 주석  추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
+// copy-webpack-plugin: favicon, img 등 정적 파일들을 빌드하는 플러그인
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
   console.log(env, options);
   return {
+    // resolve: 모듈 해석 설정을 담당한다.
     resolve: {
-      // import 키워드 사용 시 .js 확장자명을 안적어도 됨
+      // extensions: ['확장자'] 를 통해 확장자를 생략할 수 있다.
+      // ex) import 키워드 사용 시 .js 확장자명을 안적어도 됨
       extensions: ['.js'],
       alias: {
-        // 절대 경로를 넣어줌
-        // 아래 구문은 src 폴더를 기준으로 파일을 찾는 절대 경로가 된다.
+        // 경로에 대한 별칭 부여
+        // 해석: '~' 별칭을 사용해서 src 폴더 기준으로 파일을 찾는다. (절대경로)
         '~': path.resolve(__dirname, 'src') 
       }
     },
@@ -20,7 +23,7 @@ module.exports = (env, options) => {
     output: {
       // path: '',
       // filename: '',
-      // publicPath: 절대 경로를 추가한다.
+      // publicPath: 배포 빌드 시 절대 경로를 추가한다. (=prefix 개념)
       publicPath: '/',
       // clean: 파일이 초기화된다. 즉, 파일이 누적돼서 생기는 것을 방지한다.
       clean: true
