a97dd47f1d9dcfa06120345051002662c3bec06a
bxxmi <bmi6638@gmail.com>
2021-11-03 20:46:20 +0900

    feat: 모듈화

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,63 @@
 <!-- 기본 구조 : template(=html), script, style -->
 <template> 
-  <h1>
+  <h1 @click="updateMessage">
     {{ message }}
   </h1>
-  <HelloWorld class="active" />
-  <TheButton />
+  <h1>{{ reversedMessage }}</h1>
+  <input
+    v-model="message"
+    type="text" />
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld.vue';
-import TheButton from '~/components/TheButton.vue';
+import { mapGetters } from 'vuex';
 
 export default {
-  components: {
-    HelloWorld,
-    TheButton
-  },
   data() {
-    return {
-      message: 'Hello Vue'
-    }
+
   },
-  async mounted() {
-    const res = await this.$fetchMovie('frozen');
-    console.log(res);
-  }
+  computed: {
+    // ...mapState('모듈 이름', [배열 데이터])
+    // mapState는 두 개 이상의 state를 가져올 때 묶어서 가져온다.
+    // ...mapState('message', [
+    //   'message',
+    //   'msg'
+    // ]),
+    // ...mapGetter도 mapState와 역할이 동일하다.
+    ...mapGetters('message', [
+      'reversedMessage'
+    ]),
+    message: {
+      get() {
+        return this.$store.state.message.message
+      },
+      set(message) {
+        this.$store.commit('message/assignState', {
+          message
+        })
+      }
+    }
+    // message() {
+    //   // state와 message사이에 index.js 에서 지정한 모듈 이름 삽입
+    //   return this.$store.state.message.message
+    // },
+    // msg() {
+    //   return this.$store.state.message.msg
+    // },
+    // reversedMessage() {
+    //   // getters에서는 모듈을 다음과 같이 작성한다.
+    //   return this.$store.getters['message/reversedMessage']
+    // }
+  },
+  methods: {
+    // ...mapActions도 mapState와 역할이 동일하다.
+    // ...mapActions('message', [
+    //   'updateMessage'
+    // ])
+    async updateMessage() {
+      const res = await this.$store.dispatch('message/updateMessage');
+      console.log(res);
+    }
+  } 
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,51 +1,10 @@
 import { createStore } from 'vuex';
+import message from './message.js';
 
 export default createStore({
-  // state: 데이터가 담김
-  // 함수형이어야 한다.
-  state: () => ({
-    count: 0,
-    message: 'Hello Vue'
-  }),
-  // computed
-  getters: {
-    double(state) {
-      return state.count * 2;
-    },
-    reversedMessage(state) {
-      return state.message.split('').reverse.join('');
-    }
-  },
-  // 변이 메소드 (데이터 수정 권한 있음, 비동기 통신 안됨)
-  // 데이터 수정 용도이다. 그 외의 용도는 actions에 작성한다.a
-  mutations: {
-    // increase(state) {
-    //   state.count += 1
-    // },
-    // reverseMessage(state) {
-    //   state.message = state.message.split('').reverse.join('');
-    // },
-    assignState(state, bomi) { // bomi must be object! 여기서 bomi에 들어간 데이터는 actions의 commit 부분이다.
-      Object.keys(bomi).forEach(key => {
-        state[key] = bomi[key]
-      })
-    }
-  },
-  // 액션 메소드 (데이터 수정 권한 없고, 비동기 통신 됨)
-  // 개발자가 직접 사용 따라서 복잡한 로직이 존재한다.
-  actions: {
-    // 함수 - 명령형으로 생성(선택)
-    increase({ state, commit }) {
-      commit('assignState', {
-        count: state.count + 1,
-        message: 'Good'
-      })
-    },
-    reverseMessage({ state, commit }) {
-      commit('assignState', {
-        message: state.message.split('').reverse.join('')
-      })
-      console.log(state.message);
-    }
+  // strict: true 배포 시 지워야하는 코드
+  strict: true,
+  modules: {
+    message
   }
 }) 
