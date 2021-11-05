502044338e5c55be751dbd882a9dd4860f051406
bxxmi <bmi6638@gmail.com>
2021-11-05 20:30:27 +0900

    feat: 1105 데이터 depth 출력 및 제한 힌트

--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>{{ user.name }}</h1>
+  <h1>{{ user.name }} / {{ depth }}</h1>
   <!-- 자기 자신 컴포넌트를 부르면 재귀 컴포넌트가 된다. -->
   <!-- 조건을 달아야한다. 왜냐하면 재귀가 끝나는 지점이 있어야하기 때문이다. -->
   <!-- names.json 파일을 보면 children이 있고 없는 배열이 있으니 그것을 끝나는 조건으로 사용하면 된다. -->
@@ -7,7 +7,8 @@
     <UserName
       v-for="u in user.children"
       :key="u.id"
-      :user="u" />
+      :user="u"
+      :depth="depth + 1" /> 
   </template>
 </template>
 
@@ -18,6 +19,11 @@ export default {
     user: {
       type: Object,
       default: () => ({})
+    },
+    // 데이터가 저장되는 깊이 설정 (기준 이상은 생성되지 않게!)
+    depth: {
+      type: Number,
+      default: 1
     }
   }
 }
